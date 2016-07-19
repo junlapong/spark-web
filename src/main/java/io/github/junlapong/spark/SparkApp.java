a17d77aeca09744cbf40e47a0c9edbabd1fde162
Junlapong L <junlapong@gmail.com>
2016-07-19 21:26:51 +0700

    revise pom.xml

--- src/main/java/io/github/junlapong/spark/SparkApp.java
+++ src/main/java/io/github/junlapong/spark/SparkApp.java
@@ -5,26 +5,23 @@ import static spark.Spark.get;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import lombok.extern.slf4j.Slf4j;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
+@Slf4j
 public class SparkApp {
-
-    private static final Logger log = LoggerFactory.getLogger(SparkApp.class);
 
     public static void main(String[] args) {
 
-    	log.debug("***** Spark as Java App *****");
+    	log.info("***** Spark Java App *****");
 
     	spark.Spark.port(9443);
         secure("keystore.p12", "P@ssw0rd", null, null);
     	log.debug("URL: https://localhost:9443/hello");
 
     	get("/hello", (request, response) -> {
+            log.debug("hello");
     	    Map<String, Object> attributes = new HashMap<>();
     	    attributes.put("message", "Hello World!");
     	    return new ModelAndView(attributes, "hello.ftl");
